268dc769f80055665098e8d572f8e7a040a872d1
Piotr Maliński <piotr.malinski@socialwifi.com>
2017-10-23 12:34:05 +0200

    Include bootstrap files.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,7 +15,9 @@ module.exports = function(defaults) {
   */
   app.import('node_modules/moment/moment.js');
   app.import('vendor/shims/moment.js');
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
   app.import('node_modules/admin-lte/dist/css/skins/_all-skins.css');
+  app.import('node_modules/bootstrap/dist/js/bootstrap.min.js');
   app.import('node_modules/admin-lte/dist/js/adminlte.min.js');
   app.import('vendor/shims/jquery.js');
   return app.toTree();
--- index.js
+++ index.js
@@ -7,7 +7,9 @@ module.exports = {
     this._super.included(app);
     app.import('node_modules/moment/moment.js');
     app.import('vendor/shims/moment.js');
+    app.import('node_modules/bootstrap/dist/css/bootstrap.css');
     app.import('node_modules/admin-lte/dist/css/skins/_all-skins.css');
+    app.import('node_modules/bootstrap/dist/js/bootstrap.min.js');
     app.import('node_modules/admin-lte/dist/js/adminlte.min.js');
     app.import('vendor/shims/jquery.js');
   }
