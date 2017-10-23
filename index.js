0e0ddfece2dd5918adec4efdd57637490cc955f5
Piotr Maliński <piotr.malinski@socialwifi.com>
2017-10-23 11:51:23 +0200

    Don't include removed filed.

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ module.exports = {
     this._super.included(app);
     app.import('node_modules/moment/moment.js');
     app.import('vendor/shims/moment.js');
-    app.import('node_modules/admin-lte/bootstrap/css/bootstrap.css');
     app.import('node_modules/admin-lte/dist/css/skins/_all-skins.css');
-    app.import('node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js');
-    app.import('node_modules/admin-lte/bootstrap/js/bootstrap.min.js');
     app.import('node_modules/admin-lte/dist/js/adminlte.min.js');
-    app.import('node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js');
     app.import('vendor/shims/jquery.js');
   }
 };
